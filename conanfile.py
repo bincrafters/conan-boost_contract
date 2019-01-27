5f3a2146c64d4788dae3e534bb050eb3964b738a
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:59:00 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostContractConan(base.BoostBaseConan):
     name = "boost_contract"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_contract"
     lib_short_names = ["contract"]
     b2_requires = [
