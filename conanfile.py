6e741ed87c7b753f459c7a84cb2fa081328031d0
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:24 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,29 +4,13 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostContractConan(base.BoostBaseConan):
     name = "boost_contract"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_contract"
-    lib_short_names = ["contract"]
-    b2_requires = [
-        "boost_any",
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_exception",
-        "boost_function",
-        "boost_function_types",
-        "boost_mpl",
-        "boost_optional",
-        "boost_preprocessor",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_thread",
-        "boost_type_traits",
-        "boost_typeof",
-        "boost_utility"
-    ]
-    b2_build_requires = ["boost_system"]
+    version = "1.70.0"
+
+    @property
+    def boost_build_requires(self):
+        return ["system"]
